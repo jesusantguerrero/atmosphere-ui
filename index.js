9dd578b5de22db48c7bfe197352a5e7254cb1ec4
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-06-27 10:24:46 -0400

    Buid

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ import AtButton from "./src/components/atoms/AtButton/AtButton.vue";
 import AtErrorBag from "./src/components/atoms/AtErrorBag/AtErrorBag.vue";
 import AtField from "./src/components/atoms/AtField/AtField.vue";
 import AtInput from "./src/components/atoms/AtInput/AtInput.vue";
+import AtLink from "./src/components/atoms/AtLink/AtLink.vue";
 import AtTextarea from "./src/components/atoms/AtTextarea/AtTextarea.vue";
 import AtBackgroundIconCard from "./src/components/molecules/AtBackgroundIconCard/AtBackgroundIconCard.vue";
 import AtFormGroup from "./src/components/molecules/AtFormGroup/AtFormGroup.vue";
+import AtSideItem from "./src/components/molecules/AtSideItem/AtSideItem.vue";
+import AtSideItemGroup from "./src/components/molecules/AtSideItemGroup/AtSideItemGroup.vue";
+import AtWeekPager from "./src/components/molecules/AtWeekPager/AtWeekPager.vue";
 import AtAuthForm from "./src/components/organisms/AtAuthForm/AtAuthForm.vue";
 import AtDashlide from "./src/components/organisms/AtDashlide/AtDashlide.vue";
+import AtSide from "./src/components/organisms/AtSide/AtSide.vue";
 import AtAuthBox from "./src/components/templates/AtAuthBox/AtAuthBox.vue";
 
 export {
@@ -19,10 +24,15 @@ export {
   AtErrorBag,
   AtField,
   AtInput,
+  AtLink,
   AtTextarea,
   AtBackgroundIconCard,
   AtFormGroup,
+  AtSideItem,
+  AtSideItemGroup,
+  AtWeekPager,
   AtAuthForm,
   AtDashlide,
+  AtSide,
   AtAuthBox
 };

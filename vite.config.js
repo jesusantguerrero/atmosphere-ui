089df59c07daed5afb7c66a5c962e3797e3dae46
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-07-06 17:58:13 -0400

    🔧 update vite config

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', '@headlessui/vue'],
+      external: ['vue'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue'
+          vue: 'Vue',
+
         }
       }
     }

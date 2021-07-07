cbf16cc64f604f6ac632b8e3f4792e0dde76e5ff
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-07-07 09:09:23 -0400

    ✨ Add custom calendar

---
+++ src/components/molecules/AtCalendar/AtCalendar.stories.js
@@ -0,0 +1,20 @@
+import AtCalendar from "./AtCalendar.vue";
+
+export default {
+    title: 'Molecules/AtCalendar',
+    component: AtCalendar,
+    argTypes: { selected: {action: 'selected' }}
+};
+  
+const Template = (args) => ({
+components: { AtCalendar },
+setup() {
+    return { args };
+},
+template: '<div class="w-52"><at-calendar v-bind="args" v-on="args" class="w-32"/></div>',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    selected: new Date()
+}
--- src/components/molecules/AtFieldDateTime/AtFieldDateTime.vue
+++ src/components/molecules/AtFieldDateTime/AtFieldDateTime.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="flex py-1 border border-transparent divide-x" :class="{'border-red-400': isInvalid }">
-        <input v-model="date" :time="includeTime" class="w-full px-2 text-sm focus:outline-none" placeholder="mmm dd yyyy" @blur="onBlur" />
-        <input v-model="formattedTime" v-if="includeTime" :time="includeTime" @blur="onBlur" class="w-full px-2 text-sm focus:outline-none" placeholder="hh:mm a" />
+    <div class="flex w-full py-1 transition border border-transparent divide-x" :class="{'border-red-400': isInvalid, 'border-gray-400': isSelected }">
+        <input v-model="date" :time="includeTime" class="w-full px-2 text-sm focus:outline-none" placeholder="mmm dd yyyy" @blur="onBlur" ref="dateInput" @click="onFocus"/>
+        <input v-model="formattedTime" v-if="includeTime" :time="includeTime" @blur="onBlur" class="w-full px-2 text-sm focus:outline-none" placeholder="hh:mm a" ref="timeInput" @click="onFocus" />
     </div>
 </template>
 
 <script>
 import { toDate, format } from "date-fns"
 import { validateStringDate } from "../../../utils/validators/dateValidator";
-import { reactive, toRefs } from '@vue/reactivity'
-import { watch } from '@vue/runtime-core';
+import { reactive, ref, toRefs } from '@vue/reactivity'
+import { computed, watch } from '@vue/runtime-core';
 
 export default {
     props: {
@@ -28,6 +28,9 @@ export default {
         format: {
             type: String,
             default: 'MMM dd, yyyy'
+        },
+        selected: {
+            type: Boolean
         }
     },
     emits: ['update:modelValue', 'update:time'],
@@ -36,6 +39,7 @@ export default {
             date: '',
             formattedTime: '',
             isInvalid: false,
+            isFocused: false
         });
 
         const { modelValue } = toRefs(props);
@@ -53,13 +57,28 @@ export default {
         const onBlur = () => {
             state.isInvalid = !validateDate();
             if (!state.isInvalid) {
+                state.isFocused = false;
                 emit('update:modelValue', toDate(Date.parse(`${state.date} ${state.formattedTime}`)));
             }
         }
 
+        // focus
+        const dateInput = ref(null);
+        const timeInput = ref(null);
+        const onFocus = () => {
+            state.isFocused = true;
+            emit('click');
+        }
+        const isSelected = computed(() => {
+           return state.isFocused || props.selected;
+        })        
+
         return {
             ...toRefs(state),
             onBlur,
+            dateInput,
+            timeInput,
+            isSelected
         }
     },
 }

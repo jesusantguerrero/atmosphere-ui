3f4956404f2cd36a260e32dad238c87045acdc02
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-07-08 07:47:35 -0400

    ✅ Add error state for field date time

--- src/components/molecules/AtFieldDateTime/AtFieldDateTime.spec.js
+++ src/components/molecules/AtFieldDateTime/AtFieldDateTime.spec.js
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/vue";
+import { render, screen, waitFor } from "@testing-library/vue";
 import { format } from "date-fns";
 import { validateStringDate } from "../../../utils/validators/dateValidator";
+import userEvent from "@testing-library/user-event"
 import AtFieldDateTime from "./AtFieldDateTime.vue";
 
 it('returns false when the string date is not valid', () => {
@@ -20,4 +21,20 @@ it('should render the date in a valid string format', () => {
     })
 
     getByDisplayValue(format(date, 'MMM dd, yyyy'));
+})
+
+it('should display an error', async () => {
+    const date = new Date();
+    const { getByDisplayValue, getByRole } = render(AtFieldDateTime, {
+        props: {
+            modelValue: date
+        }
+    })
+
+    const dateInput = getByDisplayValue(format(date, 'MMM dd, yyyy'));
+    userEvent.type(dateInput, 'Jul 32, 2021');
+    userEvent.tab();
+    await waitFor(() => {
+        getByRole('error');
+    })
 })

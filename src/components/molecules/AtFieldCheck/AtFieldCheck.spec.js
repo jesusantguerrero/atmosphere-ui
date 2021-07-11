053ddac2e1dc4a601c8b086b5bc0baf50fbcf9c8
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-07-11 13:42:33 -0400

    ✨ Add fieldcheck component

---
+++ src/components/molecules/AtFieldCheck/AtFieldCheck.spec.js
@@ -0,0 +1,23 @@
+import AtFieldCheck from "./AtFieldCheck.vue";
+import { fireEvent, render, screen,  } from "@testing-library/vue"
+import { ref } from "@vue/runtime-core";
+
+const renderComponent = (localProps) => {
+    const state = ref(false);
+    return render(AtFieldCheck, { 
+       props: localProps || {
+            modelValue: state,
+            label: 'Test'
+        }
+    });
+}
+
+it('should emits updateValue on click', async () => {
+    const state = ref(true);
+    const { emitted } = renderComponent({ modelValue: state, label: 'test' })
+
+    const checkField = screen.getByRole('checkbox', { checked: true });
+    await fireEvent.click(checkField);
+    expect(emitted()).toHaveProperty('update:modelValue')
+    expect(emitted()['update:modelValue'][0][0]).toBe(false);
+})

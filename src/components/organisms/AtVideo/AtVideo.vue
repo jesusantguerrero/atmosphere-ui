05ee013b2aa335b68b85be5a1c9bdef32c788c15
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-08-07 17:14:31 -0400

    ✨ add video

---
+++ src/components/organisms/AtVideo/AtVideo.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="relative overflow-hidden bg-white border rounded-md">
+    <div class="absolute z-20 flex items-center justify-center w-full h-full">
+        <div class="px-5 py-2 my-auto text-white transition-transform transform bg-black cursor-pointer select-none hover:scale-105 bg-opacity-60 rounded-3xl hover:bg-opacity-80">
+            <i class="mr-2 fa fa-play" />
+            {{ label }}
+        </div>
+    </div>
+    <video 
+        loop 
+        muted 
+        playsinline="" 
+        preload="metadata" 
+        :poster="imageUrl" 
+        :width="width" 
+        :height="height"
+    >
+        <source :src="src" :type="type">
+    </video>
+</div>  
+</template>
+
+<script setup>
+defineProps({
+    width: {
+        type: Number,
+        default: 1920
+    },
+    height: {
+        type: Number,
+        default: 1080
+    },
+    src: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: 'Play video'
+    },
+    type: {
+        type: String,
+        default: 'video/mp4'
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+})
+</script>

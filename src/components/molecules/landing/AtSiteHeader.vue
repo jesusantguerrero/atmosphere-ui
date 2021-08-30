778034a91ca7c1a93d5c6441c6287999e42e43c8
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-08-29 20:57:16 -0400

    ⚡️ feat: add landing sections

---
+++ src/components/molecules/landing/AtSiteHeader.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="flex items-center h-24 mx-auto max-w-7xl">
+        <div class="z-50 flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium">
+            <component :is="routerComponent" to="/" class="py-4 pr-4 cursor-pointer">
+                <span class="text-3xl leading-none text-white select-none logo zen">{{ title }}<span class="text-green-500">.</span></span>
+            </component>
+            
+            <div class="flex-col items-center justify-end hidden w-full h-full mt-12 font-bold text-center md:flex md:w-2/3 md:mt-0 md:flex-row md:items-center">
+                <a :href="link.url" class="inline-block px-4 py-2 mx-2 text-left md:px-0 lg:mx-3 md:text-center" :class="linkClass" v-for="link in links">
+                    {{ link.name }}
+                </a>
+                <div class="md:ml-8">
+                    <component :is="routerComponent" :to="action.url" :class="action.class" class="w-full px-4 py-2 font-bold transition rounded-md cursor-pointer md:mr-3 lg:mr-3 md:w-auto" v-for="action in actions">
+                    {{ action.name }}
+                    </component>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+defineProps({
+    title: {
+        default: 'Zen',
+        type: String
+    },
+    links: {
+        type: Array,
+        default: () => {
+            return [];
+        } 
+    },
+    actions: {
+        type: Array,
+        default: () => {
+            return [];
+        }
+    },
+    linkClass: {
+        type: String,
+        default: 'text-white hover:text-green-500'
+    },
+    routerComponent: {
+        type: String,
+        default: 'router-link'
+    }
+});
+</script>

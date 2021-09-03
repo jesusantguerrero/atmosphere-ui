4ea19cc1d4aa960a8b2a4029bffb62a3e4dadd7b
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-09-03 07:58:57 -0400

    feature: add billing section cards

---
+++ src/components/molecules/AtBillingCard/AtBillingCard.stories.js
@@ -0,0 +1,22 @@
+import { ref } from 'vue';
+import AtBillingCard from './AtBillingCard.vue';
+
+export default {
+  title: 'Molecules/AtBillingCard',
+  component: AtBillingCard,
+};
+
+const Template = (args) => ({
+  components: { AtBillingCard },
+  setup() {
+    return { args };
+  },
+  template: '<AtBillingCard v-bind="args"  />',
+});
+
+
+export const Basic = Template.bind({});
+Basic.args = {
+  name: 'Starter plan',
+  status: 'active'
+};

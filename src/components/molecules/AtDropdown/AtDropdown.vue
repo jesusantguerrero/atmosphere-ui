a28910ac1577dd0f599bdbb1636b89c4413557f1
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-09-05 10:01:40 -0400

    🐛 fix autoclose dropdown

--- src/components/molecules/AtDropdown/AtDropdown.vue
+++ src/components/molecules/AtDropdown/AtDropdown.vue
@@ -5,79 +5,92 @@
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false">
+        <div v-show="open" class="fixed inset-0 z-40" @click.self="closeModal(true)">
         </div>
 
         <transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95">
-            <div v-show="open"
-                    class="absolute z-50 mt-2 rounded-md shadow-lg"
-                    :class="[widthClass, alignmentClasses]"
-                    style="display: none;"
-                    @click="open = false">
+            enter-active-class="transition duration-200 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0">
+            <div 
+                v-show="open"
+                ref="content"
+                class="absolute z-50 mt-2 rounded-md shadow-lg"
+                :class="[widthClass, alignmentClasses]"
+                style="display: none;"
+                @click.stop="closeModal(closeOnClick)">
                 <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
-                    <slot name="content"></slot>
+                    <slot name="content" :close="closeModal"></slot>
                 </div>
             </div>
         </transition>
     </div>
 </template>
 
-<script>
-import { onMounted, onUnmounted, ref } from "vue";
+<script setup>
+import { onMounted, onUnmounted, ref, computed } from "vue";
 
-export default {
-    props: {
-        align: {
-            default: 'right'
-        },
-        width: {
-            default: '48'
-        },
-        contentClasses: {
-            default: () => ['py-1', 'bg-white']
-        }
+const props = defineProps({
+    align: {
+        type: String,
+        default: 'right'
     },
-
-    setup() {
-        let open = ref(false)
-
-        const closeOnEscape = (e) => {
-            if (open.value && e.keyCode === 27) {
-                open.value = false
-            }
-        }
-
-        onMounted(() => document.addEventListener('keydown', closeOnEscape))
-        onUnmounted(() => document.removeEventListener('keydown', closeOnEscape))
-
-        return {
-            open,
-        }
+    width: {
+        type: [String, Number],
+        default: '48'
     },
+    contentClasses: {
+        type: [Array, String],
+        default: () => ['py-1', 'bg-white']
+    },
+    closeOnClick: {
+        type: Boolean,
+        default: false
+    }
+});
 
-    computed: {
-        widthClass() {
-            return {
-                '48': 'w-48',
-                'full': 'w-full'
-            }[this.width.toString()]
-        },
+const open = ref(false)
 
-        alignmentClasses() {
-            if (this.align === 'left') {
-                return 'origin-top-left left-0'
-            } else if (this.align === 'right') {
-                return 'origin-top-right right-0'
-            } else {
-                return 'origin-top'
-            }
-        },
+const closeOnEscape = (e) => {
+    if (open.value && e.keyCode === 27) {
+        open.value = false
     }
 }
+
+onMounted(() => document.addEventListener('keydown', closeOnEscape))
+onUnmounted(() => document.removeEventListener('keydown', closeOnEscape))
+
+
+const widthClass = computed(() => {
+    return {
+        '48': 'w-48',
+        'full': 'w-full'
+    }[props.width.toString()]
+});
+
+const alignmentClasses = computed(() => {
+    if (props.align === 'left') {
+        return 'origin-top-left left-0'
+    } else if (props.align === 'right') {
+        return 'origin-top-right right-0'
+    } else {
+        return 'origin-top'
+    }
+});
+
+const content = ref(null)
+const closeModal = (force) => {
+    if (!force && !props.closeOnClick) {
+        return
+    } else {
+        open.value = false;
+    }
+}
+
+defineExpose({
+    closeModal
+})
 </script>
--- src/components/molecules/AtSimpleSelect/AtSimpleSelect.vue
+++ src/components/molecules/AtSimpleSelect/AtSimpleSelect.vue
@@ -1,5 +1,5 @@
 <template>
-<dropdown align="right" width="full">
+<dropdown align="right" width="full" :close-on-click="true">
     <template #trigger>
         <span class="inline-flex w-full rounded-md">
             <button type="button" class="inline-flex items-center justify-between w-full px-3 py-3 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-gray-200 rounded-md hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50">
@@ -24,7 +24,7 @@
 
 <script setup>
 import Dropdown from '../AtDropdown/AtDropdown.vue'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps({
     options: {

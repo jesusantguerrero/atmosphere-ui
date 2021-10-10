ddc06ddc8e5adcfc451f7a6c8a583d0238abf216
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-10-10 10:37:24 -0400

    feat: improve header customization

--- src/components/molecules/landing/AtSiteHeader.vue
+++ src/components/molecules/landing/AtSiteHeader.vue
@@ -3,19 +3,33 @@
         <div class="z-50 flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium">
             <slot name="brand">
                 <component :is="routerComponent" to="/" class="py-4 pr-4 cursor-pointer" :class="brandClass">
-                    <span class="text-3xl leading-none text-white select-none logo zen">{{ title }}<span class="text-green-500">.</span></span>
+                    <span class="text-3xl leading-none text-white select-none logo zen">{{ title }}<span :class="dotClass">.</span></span>
                 </component>
             </slot>
             
             <div class="flex-col items-center justify-end hidden w-full h-full mt-12 font-bold text-center md:flex md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                <a :href="link.url" class="inline-block px-4 py-2 mx-2 text-left md:px-0 lg:mx-3 md:text-center" :class="linkClass" v-for="link in links">
+                <component 
+                    :is="link.url ? 'a' : routerComponent " 
+                    :href="link.url" 
+                    :to="link.to" 
+                    class="inline-block px-4 py-2 mx-2 text-left md:px-0 lg:mx-3 md:text-center" 
+                    :class="linkClass" v-for="link in links"
+                >
                     {{ link.name }}
-                </a>
+                </component>
                 <div class="md:ml-8">
-                    <slot v-for="action in actions" :name="action.name">
-                        <component :is="routerComponent" :to="action.url" :class="action.class" class="w-full px-4 py-2 font-bold transition rounded-md cursor-pointer md:mr-3 lg:mr-3 md:w-auto">
-                        {{ action.label || action.name }}
-                        </component>
+                    <slot name="actions">
+                        <slot v-for="action in actions" :name="action.name">
+                            <component 
+                                :is="routerComponent" 
+                                :to="action.url" 
+                                :class="action.class" 
+                                class="w-full px-4 py-2 font-bold transition rounded-md cursor-pointer md:mr-3 lg:mr-3 md:w-auto"
+                                @click.stop="handleClick($event, action)"
+                            >
+                                {{ action.label || action.name }}
+                            </component>
+                        </slot>
                     </slot>
                 </div>
             </div>
@@ -52,6 +66,19 @@ defineProps({
     routerComponent: {
         type: String,
         default: 'router-link'
+    },
+    dotClass: {
+        type: String,
+        default: "text-green-500"
     }
 });
+
+const emit = defineEmits(['action']);
+
+function handleClick = (event, action) => {
+    if (action.emit) {
+        event.preventDefault();
+        emit('action', action.name);
+    }
+}
 </script>

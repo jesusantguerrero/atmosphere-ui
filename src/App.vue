e89854dfde81697059cbc09a2fa45227f0c26f20
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-06-12 23:56:18 -0400

    feat: add support to dark themes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <at-date-pager v-model="state.date"/>
+  <AtDatePager
+      class="h-12 w-full"
+      v-model="state.date"
+      v-model:dateSpan="state.dateSpan"
+      v-model:startDate="state.searchOptions.date.startDate"
+      v-model:endDate="state.searchOptions.date.endDate"
+      next-mode="month"
+  />
 </template>
 
 <script setup>
+import { startOfDay } from "date-fns";
 import { reactive, watch } from "vue";
 import AtDatePager from "./components/molecules/AtDatePager/AtDatePager.vue"
+
+const props = defineProps({
+ serverSearchOptions: {
+    type: Object,
+    default() {
+      return {
+
+      }
+    }
+ }
+})
+
 const state = reactive({
-  date: new Date()
+  date: new Date(),
+  filterOptions: [
+      {
+          label: 'Accounts',
+          value: 'accounts'
+      },
+      {
+          label: 'Descriptions',
+          value: 'description'
+      },
+  ],
+  searchOptions: {
+      group: "",
+      date: {
+          startDate: null,
+          endDate: null,
+      }
+  },
+  date: startOfDay(new Date()),
+  dateSpan: null,
+  listType: 'table'
+})
+
+Object.entries(props.serverSearchOptions).forEach(([key, value]) => {
+    if (key === 'date') {
+        state.searchOptions.date.startDate = getDateFromIso(new Date().toISOString().slice(0, 10));
+        state.searchOptions.date.endDate = getDateFromIso(new Date().toISOString().slice(0, 10))
+        state.date = getDateFromIso(value.startDate)
+    } else {
+        state.searchOptions[key] = Object.values(state.filterOptions).map(filter => filter.value).includes(value) ? value : ""
+    }
 })
 
 watch(() => state.date, () => {

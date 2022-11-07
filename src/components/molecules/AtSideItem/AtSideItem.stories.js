ac4ee7cf0abf6440cf8e5f740010daa797453e97
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-11-07 15:13:03 -0400

    fix: install c8 for coverage

--- src/components/molecules/AtSideItem/AtSideItem.stories.js
+++ src/components/molecules/AtSideItem/AtSideItem.stories.js
@@ -3,41 +3,41 @@ import AtLink from "../../atoms/AtLink/AtLink.vue";
 import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
-  title: "Molecules/AtSideItem",
-  component: AtSideSideItem,
+    title: "Molecules/AtSideItem",
+    component: AtSideSideItem,
 };
 
 const Template = (args) => ({
-  components: { AtSideSideItem, InertiaLink: Link },
-  setup() {
-    return { args };
-  },
-  template: `<AtSideItem v-bind="args" />`,
+    components: { AtSideSideItem, InertiaLink: Link },
+    setup() {
+        return { args };
+    },
+    template: `<AtSideItem v-bind="args" />`,
 });
 
 export const Default = Template.bind({});
 Default.args = {
-  icon: "home",
-  label: "Home",
-  to: "/",
-  classes: "text-gray-400",
-  as: Link,
+    icon: "home",
+    label: "Home",
+    to: "/",
+    classes: "text-gray-400",
+    as: Link,
 };
 
 const TemplateWithCustom = (args) => ({
-  components: { AtSideSideItem, AtLink },
-  setup() {
-    return { args };
-  },
-  template: `<AtSideItem v-bind="args"> 
+    components: { AtSideSideItem, AtLink },
+    setup() {
+        return { args };
+    },
+    template: `<AtSideItem v-bind="args"> 
     <AtLink href="https://google.com">Google</AtLink> 
   </AtSideItem>`,
 });
 
 export const CustomChild = TemplateWithCustom.bind({});
 CustomChild.args = {
-  icon: "home",
-  label: "Home",
-  to: "/",
-  classes: "text-gray-400",
+    icon: "home",
+    label: "Home",
+    to: "/",
+    classes: "text-gray-400",
 };

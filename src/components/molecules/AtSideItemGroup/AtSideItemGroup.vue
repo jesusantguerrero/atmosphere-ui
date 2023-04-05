6080c1f6f2596a6d450e35b33f2028ca3ce7b796
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-04-04 23:24:28 -0400

    lint: fix types

--- src/components/molecules/AtSideItemGroup/AtSideItemGroup.vue
+++ src/components/molecules/AtSideItemGroup/AtSideItemGroup.vue
@@ -1,104 +1,100 @@
-<template>
-    <div>
-        <div
-            class="flex items-center justify-between px-5 text-left transition cursor-pointer"
-            v-bind="$attrs"
-            :class="[
-                isActive || (!isActive && hasActiveChild)
-                    ? itemActiveClass
-                    : itemClass,
-            ]"
-            @click="emitValue()"
-        >
-            <span class="w-full py-4 side-item">
-                <i :class="`fa fa-${icon} mr-2`" v-if="icon" />
-                <span v-if="isExpanded">
-                    {{ label }}
-                </span>
-            </span>
-
-            <span class="indicator">
-                <i :class="`fa fa-${arrowIcon}`" />
-            </span>
-        </div>
-
-        <div
-            class="menu-item-group__childs"
-            :class="{ 'my-collapse': isActive, 'custom-accordion': true }"
-        >
-            <div
-                class="mt-1 mb-1 ml-5 child-container space-y-1"
-                v-show="isActive"
-            >
-                <AtSideItem
-                    v-for="item in childs.filter((item) => !item.hide)"
-                    :ref="`${label}-${item.label}`"
-                    :key="`${label}-${item.label}`"
-                    :icon="item.icon"
-                    :label="item.label"
-                    :to="item.to"
-                    :as="item.as"
-                    :item-active-class="itemActiveClass"
-                    :classes="[itemClass]"
-                />
-            </div>
-        </div>
-    </div>
-</template>
-
-<script setup>
+<script lang="ts" setup>
 import { computed, inject, ref } from "vue";
 import AtSideItem from "../AtSideItem/AtSideItem.vue";
 
-const props = defineProps({
-    icon: {
-        type: String,
-        required: true,
-    },
-    label: {
-        type: String,
-        required: true,
-    },
-    childs: {
-        type: Array,
-        default() {
-            return [];
-        },
-    },
-    modelValue: {
-        type: String,
-        required: true,
-    },
-    trackId: {
-        type: String,
-        required: true,
-    },
-    itemClass: {
-        type: String,
-    },
-    itemActiveClass: {
-        type: String,
-    },
-});
+interface IMenuItem {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  icon?: string | Object;
+  name: string;
+  label: string;
+  to: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  as: string | Object;
+  hidden?: boolean;
+  isActiveFunction?: (url: string, currentPath: string) => boolean;
+}
+
+const props = defineProps<{
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  icon: string | Object;
+  label: string;
+  childs?: IMenuItem[];
+  modelValue: string;
+  trackId: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  as: string | Object;
+  itemClass;
+  string;
+  itemActiveClass: string;
+  hide: boolean;
+}>();
 
 const isExpanded = inject("isExpanded");
 const currentPath = inject("currentPath", ref(""));
 
 const isActive = computed(() => {
-    return props.trackId == props.modelValue;
+  return props.trackId == props.modelValue;
 });
 const hasActiveChild = computed(() => {
-    return (
-        !isActive.value && props.childs.find((item) => item.to == currentPath.value)
-    );
+  return (
+    !isActive.value && props.childs.find((item) => item.to == currentPath.value)
+  );
 });
 const arrowIcon = computed(() => {
-    return isActive.value ? "chevron-down" : "chevron-right";
+  return isActive.value ? "chevron-down" : "chevron-right";
 });
 
 const emit = defineEmits(["update:modelValue"]);
 const emitValue = () => {
-    const current = props.modelValue == props.trackId ? "" : props.trackId;
-    emit("update:modelValue", current);
+  const current = props.modelValue == props.trackId ? "" : props.trackId;
+  emit("update:modelValue", current);
+};
+
+const isIconComponent = () => {
+  return typeof props.icon !== "string";
 };
 </script>
+
+<template>
+  <div>
+    <div
+      class="flex items-center justify-between px-5 text-left transition cursor-pointer"
+      v-bind="$attrs"
+      :class="[
+        isActive || (!isActive && hasActiveChild) ? itemActiveClass : itemClass,
+      ]"
+      @click="emitValue()"
+    >
+      <span class="w-full py-4 side-item">
+        <component :is="icon" class="mr-2" v-if="isIconComponent" />
+        <i :class="`fa fa-${icon} mr-2`" v-else-if="icon" />
+        <span v-if="isExpanded">
+          {{ label }}
+        </span>
+      </span>
+
+      <span class="indicator">
+        <i :class="`fa fa-${arrowIcon}`" />
+      </span>
+    </div>
+
+    <div
+      class="menu-item-group__childs"
+      :class="{ 'my-collapse': isActive, 'custom-accordion': true }"
+    >
+      <div class="mt-1 mb-1 ml-5 space-y-1 child-container" v-show="isActive">
+        <AtSideItem
+          v-for="item in childs.filter((item) => !item.hidden)"
+          :ref="`${label}-${item.label}`"
+          :key="`${label}-${item.label}`"
+          :icon="item.icon"
+          :label="item.label"
+          :to="item.to"
+          :as="item.as as string"
+          :item-active-class="itemActiveClass"
+          :classes="itemClass"
+        />
+      </div>
+    </div>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -6,50 +6,47 @@ import postcss from "./postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
-    css: {
-        postcss,
+  plugins: [vue(), vueJsx()],
+  css: {
+    postcss,
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL("src/index.js", import.meta.url)),
+      name: "atmosphere-ui",
     },
-    build: {
-        lib: {
-            entry: fileURLToPath(new URL("src/index.js", import.meta.url)),
-            name: "atmosphere-ui",
+    rollupOptions: {
+      external: ["vue", "@vueuse/core"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
         },
-        rollupOptions: {
-            external: [
-                "vue",
-                "@vueuse/core",
-            ],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: "Vue",
-                },
-            },
-        },
+      },
     },
-    resolve: {
-        alias: {
-            "~/": fileURLToPath(new URL("./src", import.meta.url)),
-            "~atoms": fileURLToPath(
-                new URL("./src/components/atoms", import.meta.url)
-            ),
-            "~molecules": fileURLToPath(
-                new URL("./src/components/molecules", import.meta.url)
-            ),
-            "~organisms": fileURLToPath(
-                new URL("./src/components/organisms", import.meta.url)
-            ),
-            "~templates": fileURLToPath(
-                new URL("./src/components/templates", import.meta.url)
-            ),
-            "~utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
-        },
+  },
+  resolve: {
+    alias: {
+      "~/": fileURLToPath(new URL("./src", import.meta.url)),
+      "~atoms": fileURLToPath(
+        new URL("./src/components/atoms", import.meta.url)
+      ),
+      "~molecules": fileURLToPath(
+        new URL("./src/components/molecules", import.meta.url)
+      ),
+      "~organisms": fileURLToPath(
+        new URL("./src/components/organisms", import.meta.url)
+      ),
+      "~templates": fileURLToPath(
+        new URL("./src/components/templates", import.meta.url)
+      ),
+      "~utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
     },
-    test: {
-        include: ["src/**/*.spec.*"],
-        environment: "jsdom",
-        globals: true,
-    },
+  },
+  test: {
+    include: ["src/**/*.spec.*"],
+    environment: "jsdom",
+    globals: true,
+  },
 });
